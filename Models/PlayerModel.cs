df7afdd33d60c5f41b78b763291c270167a43270
Ege Erdağ <egeerdag@gmail.com>
2024-07-27 17:15:58 +0300

    create css files to style view pages

--- Models/PlayerModel.cs
+++ Models/PlayerModel.cs
@@ -6,6 +6,7 @@ namespace basketball_tournament_tracker.Models
     {
         public ObjectId Id { get; set; }
         public string Name { get; set; }
+        public string Team { get; set; }
         public int JerseyNumber { get; set; }
         public string Position { get; set; }
 
